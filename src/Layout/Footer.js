285d5a9d4cf6b6172bcf5e65327504f51dff363a
Deven Dattani <devzi100@icloud.com>
2024-06-01 16:15:15 +0100

    Layout, Header, Footer added

---
+++ src/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../CSS/Layout.css'
+
+function Footer() {
+    return (
+      <div className="Footer">
+          <h1>Footer goes here</h1>
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
