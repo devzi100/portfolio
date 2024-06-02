be39b4e35061ef43fc1d8d99c5601c19533333d0
Deven Dattani <devzi100@icloud.com>
2024-06-02 04:12:21 +0100

    Footer Intial Version

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,12 +1,54 @@
 import React from "react";
-import '../CSS/Layout.css'
+import "../CSS/Layout.css";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import EmailIcon from '@mui/icons-material/Email';
 
 function Footer() {
-    return (
-      <div className="Footer">
-      </div>
-    );
-  }
-  
-  export default Footer;
-  
+
+  return (
+    <div className="Footer">
+      <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+        <BottomNavigation showLabels>
+
+          <BottomNavigationAction
+            label="View my Github Profile"
+            icon={
+              <img
+              src="GitHub.svg"
+              alt="Github Invertocat Logo"
+              style={{ width: "auto", height: 24 }}
+            />
+            }
+            component={Link}
+            to="https://github.com/devzi100"
+          />
+
+          <BottomNavigationAction
+            label="Send me an Email"
+            icon={<EmailIcon />}
+            component={Link}
+            to="mailto:contact@devendattani.com"
+          />
+
+          <BottomNavigationAction
+            label="View my LinkedIn&reg; Profile"
+            icon={
+              <img
+                src="LinkedIn.png"
+                alt="LinkedIn In Logo"
+                style={{ width: "auto", height: 24 }}
+              />
+            }
+            component={Link}
+            to="https://www.linkedin.com/in/devendattani/"
+          />
+        </BottomNavigation>
+      </Box>
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './Pages/Home'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./CSS/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import Home from "./Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
-  }
-])
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
