2120f058e0ddd293d89956e805472c33507e03b1
Deven <dxd241@student.bham.ac.uk>
2024-06-04 21:47:21 +0100

    Responsive Header Added

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,41 +1,150 @@
 import React, { useContext } from "react";
-import { AppBar, IconButton, Toolbar, Typography, Tooltip } from "@mui/material";
+import {
+  IconButton,
+  Tooltip,
+  Box,
+  Button,
+  Grid,
+  Typography,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import { themeToggle } from "../index";
 import { useTheme } from "@emotion/react";
+import MenuIcon from "@mui/icons-material/Menu";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { useNavigate } from "react-router-dom";
 
+const pages = ["Home", "Experience", "Projects", "Contact"];
 
 function Header() {
+  const [navMenu, setNavMenu] = React.useState(null);
   const { toggleColorMode } = useContext(themeToggle);
+
+  const handleOpenNavMenu = (event) => {
+    setNavMenu(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setNavMenu(null);
+  };
   const theme = useTheme();
+  const logo =
+    theme.palette.mode === "dark"
+      ? "/Favicon/logo-darkmode.svg"
+      : "/Favicon/logo.svg";
+  const themeToggleTitle =
+    theme.palette.mode === "dark"
+      ? "Switch to Light Mode"
+      : "Switch to Dark Mode";
+  const navigate = useNavigate();
+  const handleNavigation = (page) => {
+    page = page.toLowerCase() === "home" ? "" : page;
+    navigate(`/${page.toLowerCase()}`);
+  };
 
   return (
-    <AppBar sx={{
-      backgroundColor: theme.palette.background.default, color: theme.palette.text.primary
-      }}>
-        <Toolbar disableGutters>
+    <Grid
+      container
+      alignItems="center"
+      overflow={"auto"}
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+      }}
+    >
+      <Box
+        sx={{
+          display: { xs: "inline", sm: "none" },
+        }}
+      >
+        <IconButton
+          aria-label="Navigation bar"
+          aria-controls="navigation-bar"
+          onClick={handleOpenNavMenu}
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="navigation-bar"
+          anchorEl={navMenu}
+          open={Boolean(navMenu)}
+          onClose={handleCloseNavMenu}
+        >
+          {pages.map((page) => (
+            <MenuItem key={page} onClick={() => handleNavigation(page)}>
+              <Typography textAlign="center">{page}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+
+      <Box
+        sx={{
+          textAlign: { xs: "center", sm: "left" },
+          flexGrow: 1,
+          margin: { xs: "0 auto" },
+        }}
+      >
+        <IconButton onClick={() => handleNavigation("home")}>
           <img
-            src={theme.palette.mode === 'dark' ? '/Favicon/logo-darkmode.svg' : '/Favicon/logo.svg'}
+            src={logo}
             alt="DD Logo"
-            style={{ width: 40, height: 40, justifyContent: 'flex-start'}}
+            style={{
+              width: 40,
+              height: 40,
+              mr: 1,
+            }}
           />
+        </IconButton>
+      </Box>
 
-          <Typography
-            component="a"
-            href="/"
+      <Box sx={{ display: { xs: "none", sm: "inline" } }}>
+        {pages.map((page) => (
+          <Button
+            key={page}
+            onClick={() => handleNavigation(page)}
+            sx={{ my: 2, color: "inherit" }}
           >
-            Deven Dattani
-          </Typography>
+            {page}
+          </Button>
+        ))}
+        <Tooltip
+          title={
+            theme.palette.mode === "dark"
+              ? "Switch to Light Mode"
+              : "Switch to Dark Mode"
+          }
+        >
+          <IconButton className="LightDarkToggle" onClick={toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <Brightness7Icon />
+            ) : (
+              <Brightness4Icon />
+            )}
+          </IconButton>
+        </Tooltip>
+      </Box>
 
-            <Tooltip title={theme.palette.mode === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode'}>
-              <IconButton className="LightDarkToggle" onClick={toggleColorMode}>
-                {theme.palette.mode === "dark" ? (<Brightness7Icon />) : (<Brightness4Icon />)}
-              </IconButton>
-            </Tooltip>
-
-        </Toolbar>
-    </AppBar>
+      <Box
+        sx={{
+          textAlign: "right",
+          display: { xs: "inline", sm: "none" },
+        }}
+      >
+        <Tooltip title={themeToggleTitle}>
+          <IconButton className="LightDarkToggle" onClick={toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <Brightness7Icon />
+            ) : (
+              <Brightness4Icon />
+            )}
+          </IconButton>
+        </Tooltip>
+      </Box>
+    </Grid>
   );
 }
 export default Header;
